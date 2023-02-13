aea9de2350f045ae02bab937f9c03b5033a4844b
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-13 22:05:05 +0700

    refactor: change login response

--- user-service/src/main/java/com/example/userservice/infrastructure/presenter/rest/authentication/dto/AuthenticationResponse.java
+++ user-service/src/main/java/com/example/userservice/infrastructure/presenter/rest/authentication/dto/AuthenticationResponse.java
@@ -1,8 +1,11 @@
 package com.example.userservice.infrastructure.presenter.rest.authentication.dto;
 
+import com.example.userservice.application.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +13,16 @@ import lombok.NoArgsConstructor;
 public class AuthenticationResponse {
     private String token_type = "Bearer";
     private String token;
+    private HashMap<String, Object> user = new HashMap<>();
     public AuthenticationResponse(String token){
         this.token = token;
+    }
+
+    public void setUser(User authenticatedUser) {
+        user.put("id", authenticatedUser.getId());
+        user.put("username", authenticatedUser.getUsername());
+        user.put("email", authenticatedUser.getEmail());
+        user.put("fullname", authenticatedUser.getFullname());
+        user.put("role", authenticatedUser.getRole());
     }
 }
