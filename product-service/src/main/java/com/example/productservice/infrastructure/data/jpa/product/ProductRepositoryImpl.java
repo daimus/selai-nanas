df32df6e2769176a7c5c5804e3871bcedb4feb7c
reski-id <pesan.reski@gmail.com>
2023-02-14 21:19:49 +0700

    unit test

--- product-service/src/main/java/com/example/productservice/infrastructure/data/jpa/product/ProductRepositoryImpl.java
+++ product-service/src/main/java/com/example/productservice/infrastructure/data/jpa/product/ProductRepositoryImpl.java
@@ -102,4 +102,9 @@ public class ProductRepositoryImpl implements ProductRepository {
         return true;
     }
 
+    @Override
+    public Object existsById(Long expectedId) {
+        return null;
+    }
+
 }
--- user-service/src/test/java/com/example/userservice/UserServiceTests.java
+++ user-service/src/test/java/com/example/userservice/UserServiceTests.java
@@ -4,9 +4,11 @@ import com.example.userservice.application.user.entity.User;
 import com.example.userservice.application.user.repository.UserRepository;
 import com.example.userservice.application.user.service.UserService;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
@@ -14,6 +16,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserServiceTests {
@@ -21,6 +24,26 @@ class UserServiceTests {
     UserRepository userRepository;
     @InjectMocks
     UserService userService;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("john");
+        user.setPassword("password");
+    }
+//    @Test
+//    public void getUsers_ShouldReturnListOfUsers() {
+//        List<User> users = new ArrayList<>();
+//        users.add(new User(1L, "John", "1234", "jhon do", "email@gmail.com", null, "user", "jakarta", null, null, null));
+//
+//        when(userRepository.findAll()).thenReturn(users);
+//
+//        List<User> actualUsers = userService.getUsers();
+//
+//        assertEquals(users, actualUsers);
+//    }
+
     @Test
     public void getUsers_WhenUsersExist_ShouldReturnListOfProduct() {
         EasyRandom easyRandom = new EasyRandom();
