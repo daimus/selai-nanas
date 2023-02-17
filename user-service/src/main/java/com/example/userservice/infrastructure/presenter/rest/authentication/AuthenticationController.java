a240d80b35dc4f7c77a64e729f20ee2c822a3aa0
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-18 00:44:33 +0700

    change login path

--- user-service/src/main/java/com/example/userservice/infrastructure/presenter/rest/authentication/AuthenticationController.java
+++ user-service/src/main/java/com/example/userservice/infrastructure/presenter/rest/authentication/AuthenticationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("login")
+@RequestMapping("/users/login")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
