9cd47ff0ac69309eb4f8b72d357a6579beed61b1
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-23 09:44:45 +0700

    test cors

--- order-service/src/main/java/com/example/orderservice/infrastructure/presenter/rest/config/WebConfig.java
+++ order-service/src/main/java/com/example/orderservice/infrastructure/presenter/rest/config/WebConfig.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+        .allowedMethods("*")
+        .allowedOrigins("*")
+        .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization")
+        .allowCredentials(true);
     }
 }
