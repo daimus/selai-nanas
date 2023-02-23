6bebb27e83d9eece89cf62256b46153cb1b2564d
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-02-23 10:48:09 +0700

    c

--- order-service/src/main/java/com/example/orderservice/infrastructure/presenter/websocket/WebSocketConfig.java
+++ order-service/src/main/java/com/example/orderservice/infrastructure/presenter/websocket/WebSocketConfig.java
@@ -5,9 +5,11 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.stereotype.Controller;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Controller
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
